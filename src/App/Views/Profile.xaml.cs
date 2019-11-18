adf1a493c47e70e47095807d02c1d6e067851a5a
Pablo Piovano <ppiova@hotmail.com>
2019-11-18 15:55:58 -0300

    Add - Tab and Views

---
+++ src/App/Views/Profile.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace App.Views
+{
+    public partial class Profile : ContentPage
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+    }
+}
