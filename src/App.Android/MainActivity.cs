c830a5b4659da93925aac57927811487023708d5
Steven Alexander Checo Terrero <steven.checo.19@gmail.com>
2019-11-21 03:35:35 -0500

    Set NoActionBar in Android Styles to set StatusBarTextColor Black and set StatusBarBackGround color like app background in MainActivity.cs

--- src/App.Android/MainActivity.cs
+++ src/App.Android/MainActivity.cs
@@ -22,6 +22,8 @@ namespace App.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#F8F8F8"));
+            
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
