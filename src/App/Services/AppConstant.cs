e029cf27e5c2d2ab1e86ffb56732120a875856a7
Steven Alexander Checo Terrero <steven.checo.19@gmail.com>
2019-11-27 12:37:11 -0500

    Setting AppSecrets to "Empleado-Apikey"

--- src/App/Services/AppConstant.cs
+++ src/App/Services/AppConstant.cs
@@ -11,7 +11,7 @@ namespace App.Services
     public class AppConstant
     {
         public const string ApiUrl = "https://emplea-apm.azure-api.net/v1/api";
-        public const string AppSecrets = "155d4cd48bb34ba0b8375fd50b779b85";
+        public const string AppSecrets = "Emplado-ApiKey";
         public const string ApiEndPoint = "jobs";
         public const int PageSize = 10;
 
