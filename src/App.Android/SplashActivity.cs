4f42a7bc93184f670f23a062b720a7cf3168486b
Claudio Sanchez <claudio@megsoftconsulting.com>
2019-11-28 15:35:07 -0500

    Adding App icon to Android App (#16)

---
+++ src/App.Android/SplashActivity.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App.Droid
+{
+    [Activity(Label = "Splash", Icon = "@mipmap/Icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+        }
+    }
+}
