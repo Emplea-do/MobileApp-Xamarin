fe2976ce006235340fa4e3f5b9cca93e296fe97c
Pablo Piovano <ppiova@gmail.com>
2019-11-28 02:47:55 -0300

    Modify ResourceDictionary
    Add Colors SearchView

---
+++ src/App/Services/AppConstant.cs
@@ -0,0 +1,22 @@
+﻿using Flurl;
+using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App.Services
+{
+    public class AppConstant
+    {
+        public const string ApiUrl = "https://emplea-apm.azure-api.net/v1/api";
+        public const string AppSecrets = "Empleado-ApiKey";
+        public const string ApiEndPoint = "jobs";
+        public const int PageSize = 10;
+
+
+        public static readonly Color Background = Color.FromHex("F8F8F8");
+
+    }
+}
