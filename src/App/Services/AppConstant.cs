ddbd186700ba6448eaade09136a07c3dbbf299d6
Steven Alexander Checo Terrero <steven.checo.19@gmail.com>
2019-11-27 23:56:53 -0500

    Adding appcenter-pre-build.sh file

--- src/App/Services/AppConstant.cs
+++ src/App/Services/AppConstant.cs
@@ -11,7 +11,7 @@ namespace App.Services
     public class AppConstant
     {
         public const string ApiUrl = "https://emplea-apm.azure-api.net/v1/api";
-        public const string AppSecrets = "Emplado-ApiKey";
+        public const string AppSecrets = "Empleado-ApiKey";
         public const string ApiEndPoint = "jobs";
         public const int PageSize = 10;
 
