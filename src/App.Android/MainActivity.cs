a5f2eb496b568265338982ad2d740a5fa99152b7
Claudio Sanchez <claudio@megsoftconsulting,com>
2019-11-28 15:13:17 -0500

    created splash activity

--- src/App.Android/MainActivity.cs
+++ src/App.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using App.Services;
 
 namespace App.Droid
 {
-    [Activity(Label = "App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
