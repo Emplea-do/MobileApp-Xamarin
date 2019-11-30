94a7c94aec856c133f863c288de226b5874dccad
Claudio Sanchez <claudio@megsoftconsulting.com>
2019-11-29 23:06:55 -0500

    Small UI tweaks (#23)

--- src/App/Services/AppConstant.cs
+++ src/App/Services/AppConstant.cs
@@ -11,9 +11,9 @@ namespace App.Services
     public class AppConstant
     {
         public const string ApiUrl = "https://emplea-apm.azure-api.net/v1/api";
-        public const string AppSecrets = "Empleado-ApiKey";
+        public const string AppSecrets = "3f5b06a8cbd042a4b9963da0c41fb00e";
         public const string ApiEndPoint = "jobs";
-        public const int PageSize = 10;
+        public const int PageSize = 50;
 
 
         public static readonly Color Background = Color.FromHex("F8F8F8");
