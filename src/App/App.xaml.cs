6053ce95ef3e542b784c4bbbc86766cc5c4fae98
AdrianaMota <lishamota@hotmail.com>
2019-12-05 03:02:57 -0400

    Undo AppConstants Settings - Fixed Resource dictionary

--- src/App/App.xaml.cs
+++ src/App/App.xaml.cs
@@ -11,7 +11,7 @@ namespace App
             InitializeComponent();
 
             // MainPage = new MainPage();
-            MainPage = new Views.JobDetailView();
+            MainPage = new MainShell();
         }
 
         protected override void OnStart()
