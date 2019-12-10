763974910188d3f806f6992a3212cb036ff76235
Pablo Piovano <ppiova@hotmail.com>
2019-12-10 00:21:26 -0300

    merge with master

--- src/App/Services/AppConstant.cs
+++ src/App/Services/AppConstant.cs
@@ -11,7 +11,7 @@ namespace App.Services
     public class AppConstant
     {
         public const string ApiUrl = "https://emplea-apm.azure-api.net/v1/api";
-        public const string AppSecrets = "Emplea.do-API";
+        public const string AppSecrets = "3f5b06a8cbd042a4b9963da0c41fb00e";
         public const string ApiEndPoint = "jobs";
         public const int PageSize = 50;
 
