ee7f7394e76d905f778e1aa20a8018b1b4848d48
Pablo Piovano <ppiova@gmail.com>
2020-03-27 00:53:30 -0300

    add parameters Search

--- src/App/ViewModels/JobsListViewModel.cs
+++ src/App/ViewModels/JobsListViewModel.cs
@@ -3,34 +3,48 @@ using App.Services;
 using App.Views;
 using Flurl;
 using Flurl.Http;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace App.ViewModels
 {
-    class JobsListViewModel : INotifyPropertyChanged
+    [QueryProperty("Parameters", "parameters")]
+    public class JobsListViewModel : INotifyPropertyChanged
     {
+
+
         //public AppConstant ServiceConstant = new AppConstant();
         public JobListModel JobsList { get; set; }
         public Command CallDetailScreenCommand { get; set; }
         public INavigation Navigation { get; set; }
-        public JobsListViewModel(INavigation navshell)
+
+
+        public string Parameters
         {
-            this.Navigation = navshell;
-            CallDetailScreenCommand = new Command<string>(async (string Link) => await OpenDetailViewAsync(Link));
-            new Action(async () => await LoadDataAsync())();
+            set
+            {
+                        
+                var vm = Task.Run(() => JsonConvert.DeserializeObject<ParametersSearch>(Uri.UnescapeDataString(value))).Result;
 
-            
+                var parametersSearch = new ParametersSearch
+                {
+                    EntryKeyWord = vm.EntryKeyWord,
+                    Category = vm.Category,
+                    IsRemote = vm.IsRemote
+                };
+            }
+
         }
-
         public async Task LoadDataAsync()
         {
-            
+
             var Cards = await AppConstant.ApiUrl
                 .AppendPathSegment(AppConstant.ApiEndPoint)
                 .SetQueryParams(new { pagesize = AppConstant.PageSize, page = 1 })
@@ -40,11 +54,28 @@ namespace App.ViewModels
 
         }
 
-        public async Task OpenDetailViewAsync(string link) {
-            
-            await Navigation.PushAsync(new JobDetailView(link));
-        
+
+        public JobsListViewModel(INavigation navshell)
+        {
+            this.Navigation = navshell;
+            CallDetailScreenCommand = new Command<string>(async (string Link) => await OpenDetailViewAsync(Link));
+            new Action(async () => await LoadDataAsync())();
+
+
         }
+
+        public async Task OpenDetailViewAsync(string link)
+        {
+
+            await Navigation.PushAsync(new JobDetailView(link));
+
+        }
+
+       
+   
+
         public event PropertyChangedEventHandler PropertyChanged;
+
     }
+   
 }
