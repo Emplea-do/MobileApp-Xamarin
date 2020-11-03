30ba294c427be9cf1e20488cd8cd5b40c3ad8a24
Gabriel Eduardo Acosta Nuñez <gacosta@confisa.do>
2020-11-03 16:14:08 -0400

    agregué prism nugets and key para consumir el api

--- src/App/Services/AppConstant.cs
+++ src/App/Services/AppConstant.cs
@@ -10,11 +10,13 @@ namespace App.Services
 {
     public class AppConstant
     {
-        public const string ApiUrl = "URL";
-        public const string AppSecrets = "ApiKey";
+        public const string ApiUrl = "https://emplea-apm.azure-api.net/v1/api";
+        public const string AppSecrets = "3f5b06a8cbd042a4b9963da0c41fb00e";
         public const string ApiEndPointSearch = "jobs/search";
         public const string ApiEndPointDetail = "jobs/detail/";
-        public const int PageSize = 50;
+        public const int PageSize = 50; 
+        public const string ApiEndPoint = "jobs";
+      
 
 
         public static readonly Color Background = Color.FromHex("F8F8F8");
