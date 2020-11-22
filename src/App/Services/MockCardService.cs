39602d07df7e4416ca62b7dd05a9b40c399149d5
Steven Checo <steven.checo.19@gmail.com>
2020-11-22 11:52:08 -0500

    fixed mockdata

--- src/App/Services/MockCardService.cs
+++ src/App/Services/MockCardService.cs
@@ -13,15 +13,15 @@ namespace App.Services
         {
             return new List<JobCards>
             {
-                new JobCards { Id=1, NameCard = "Diseño Gráfico", Image = "graphicdesing.png" },
-                new JobCards { Id=2, NameCard = "Desarrollo Web", Image = "web.png"},
-                new JobCards { Id=3, NameCard = "Desarrollo para Móviles", Image = "mobile.png" },
-                new JobCards { Id=4, NameCard = "Desarrollo para Software", Image = "software.png"},
-                new JobCards { Id=5, NameCard = "Administrador de Sistemas", Image = "systems.png"},
-                new JobCards { Id=6, NameCard = "Redes y telecomunicaciones", Image = "networks.png"},
-                new JobCards { Id=7, NameCard = "IT Ventas", Image = "sales.png"},
-                new JobCards { Id=8, NameCard = "Administrador de Base de Datos", Image = "database.png"},
-                new JobCards { Id=9, NameCard = "Desarrollo de Videojuegos", Image = "videogames.png"}
+                new JobCards { Id=1, NameCard = "Diseño\nGráfico", Image = "graphicdesing.png" },
+                new JobCards { Id=2, NameCard = "Desarrollo\nWeb", Image = "web.png"},
+                new JobCards { Id=3, NameCard = "Desarrollo\nMóviles", Image = "mobile.png" },
+                new JobCards { Id=4, NameCard = "Desarrollo\nde Software", Image = "software.png"},
+                new JobCards { Id=5, NameCard = "Administrador\nde Sistemas", Image = "systems.png"},
+                new JobCards { Id=6, NameCard = "Redes y\nTelecomunicaciones", Image = "networks.png"},
+                new JobCards { Id=7, NameCard = "IT\nVentas", Image = "sales.png"},
+                new JobCards { Id=8, NameCard = "Administrador\nBase de Datos", Image = "database.png"},
+                new JobCards { Id=9, NameCard = "Desarrollo de\nVideojuegos", Image = "videogames.png"}
             };
         }
     }
