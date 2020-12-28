a77e96d7c9a736c5b3ae13cda781cafd95020dfd
Piova <ppiova@gmail.com>
2020-12-28 20:27:21 -0300

    Modification Build
    Modification in Build for AppCenter

--- src/App/appcenter-pre-build.sh
+++ src/App/appcenter-pre-build.sh
@@ -1,24 +1,3 @@
-#!/usr/bin/env bash
-#
-# For Xamarin, change some constants located in some class of the app.
-# In this sample, suppose we have an AppConstant.cs class in shared folder with follow content:
-#
-# namespace Core
-# {
-#     public class AppConstant
-#     {
-#         public const string ApiUrl = "https://CMS_MyApp-Eur01.com/api";
-#     }
-# }
-# 
-# Suppose in our project exists two branches: master and develop. 
-# We can release app for production API in master branch and app for test API in develop branch. 
-# We just need configure this behaviour with environment variable in each branch :)
-# 
-# The same thing can be perform with any class of the app.
-#
-# AN IMPORTANT THING: FOR THIS SAMPLE YOU NEED DECLARE API_URL ENVIRONMENT VARIABLE IN APP CENTER BUILD CONFIGURATION.
-
 if [ -z "$API_URL" ]
 then
     echo "You need define the API_URL variable in App Center"
@@ -42,7 +21,7 @@ then
 fi
 
 
-APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/App/Services/AppConstant.cs
+APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/src/App/Services/AppConstant.cs
 
 if [ -e "$APP_CONSTANT_FILE" ]
 then
@@ -56,4 +35,6 @@ then
     sed -i '' 's#AppCenterAndroid = "[-A-Za-z0-9:_./]*"#AppCenterAndroid = "'$APP_CENTERANDROID'"#' $APP_CONSTANT_FILE
     echo "Updating AppCenterAndroid to $APP_CENTERANDROID in AppConstant.cs"
     
+    echo "File content:"
+    cat $APP_CONSTANT_FILE
 fi
